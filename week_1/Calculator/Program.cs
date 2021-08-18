14e28c043b11c2b490c1b1f0714512bc4536c350
fred belotte <fred.belotte@revature.net>
2021-08-18 14:00:11 -0500

    add calculator project

---
+++ week_1/Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿// Topics Discussed
+
+// Type Inference (var)
+// Parsing
+// Casting (implicit, explicit)
+// Scopes (namespace, class, method, block)
+// Single Responsibility (part of SOLID)
+// DRY (don't repeat yourself)
+// Method (signature, parameter, argument)
+// Primitive Types (int, bool, string, float, double)
+// Type Families (value, reference)
+
+using System;
+
+namespace Calculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // input stuff
+      var input1 = int.Parse(Console.ReadLine()); //123a, 123 // type inference, parsing
+      var input2 = int.Parse(Console.ReadLine());
+
+      // compute stuff
+      int result1 = Add(input1, input2);
+      int result2 = Subtract(input1, input2);
+
+      // output stuff
+      Print(result1, result2, result1, result2);
+    }
+
+    static void Add(int input1, int input2)
+    {
+      // compute stuff
+      var compute = (int)input1 + (int)input2; // type inference, casting
+    }
+
+    static void Subtract(int input1, int input2)
+    {
+      // compute stuff
+      var compute = (int)input1 - (int)input2; // type inference, casting
+    }
+
+    static void Print(params int[] results)
+    {
+      // output stuff
+      Console.WriteLine(result1, result2);
+    }
+  }
+}
---
+++ week_1/HelloCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloCsharp
+{
+  class Program
+  {
+    // Build a simple calculator using 5 methods: Add, Multiply, Subtract, Divide, Print
+
+    static void Main(string[] args)
+    {
+
+    }
+
+    int Add(int a, int b)
+    {
+      return a + b;
+    }
+  }
+}
